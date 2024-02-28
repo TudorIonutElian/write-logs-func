32cc94a54d6f95382408c0deaa2c3deedcfb87aa
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-28 23:30:39 +0200

    Oneline script

--- templates/sqlTemplates.js
+++ templates/sqlTemplates.js
@@ -13,19 +13,6 @@ const sqlTemplates = {
 
 sqlTemplates.createDatabase = `CREATE DATABASE IF NOT EXISTS \`cloudwatch_logs\`;`;
 sqlTemplates.useDatabase = `USE \`cloudwatch_logs\`;`;
-sqlTemplates.createTable = `USE \`cloudwatch_logs\`; CREATE TABLE IF NOT EXISTS logs (
-	id INT PRIMARY KEY auto_increment NOT NULL,
-	requestType VARCHAR(10) NOT NULL,
-	requestUrl VARCHAR(10) NOT NULL,
-	requestIp VARCHAR(10) NOT NULL,
-	requestVpc VARCHAR(50) NOT NULL,
-	requestRegion VARCHAR(50) NOT NULL,
-	requestAvailabilityZone VARCHAR(50) NOT NULL,
-	requestIamRole VARCHAR(50) NOT NULL,
-	requestApiKey VARCHAR(50) NOT NULL,
-	requestUsername VARCHAR(50) NOT NULL,
-	requestAuthorizationPolicy BOOLEAN NOT NULL DEFAULT true,
-	requestScanned boolean NOT NULL DEFAULT true
-);`;
+sqlTemplates.createTable = `USE \`cloudwatch_logs\`; CREATE TABLE IF NOT EXISTS logs (id INT PRIMARY KEY auto_increment NOT NULL, requestType VARCHAR(10) NOT NULL, requestUrl VARCHAR(10) NOT NULL, requestIp VARCHAR(10) NOT NULL, requestVpc VARCHAR(50) NOT NULL, requestRegion VARCHAR(50) NOT NULL, requestAvailabilityZone VARCHAR(50) NOT NULL, requestIamRole VARCHAR(50) NOT NULL, requestApiKey VARCHAR(50) NOT NULL, requestUsername VARCHAR(50) NOT NULL, requestAuthorizationPolicy BOOLEAN NOT NULL DEFAULT true, requestScanned boolean NOT NULL DEFAULT true);`;
 
 module.exports = { sqlTemplates };
