20ad545931ce25339ad95873d565e7cd643279b4
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-28 22:02:33 +0200

    Write object outside

--- functions/downloadFile.js
+++ functions/downloadFile.js
@@ -19,6 +19,7 @@ const downloadFileFromS3 = async (fileKey) => {
         const response = await s3.send(command);
         const str = await response.Body.transformToString();
         const strArray = str.split('\n');
+        const fullObjectWrite = {};
         
         console.log(`Lines array: ${JSON.stringify(strArray)}`);
        
@@ -26,10 +27,11 @@ const downloadFileFromS3 = async (fileKey) => {
             const lineSplit = line.split(', ');
             const lineKeyValue = lineSplit.reduce((acc, curr) => {
                 const [key, value] = curr.split(':');
-                return { ...curr, [key]: value };
+                fullObjectWrite[key] = value;
             });
-            console.log(`Line lineKeyValue: ${JSON.stringify(lineKeyValue)}`);
         });
+
+        console.log(`Line lineKeyValue: ${JSON.stringify(fullObjectWrite)}`);
         
         const tmpDirectory = '/tmp';
         const temporaryFilePath = `${tmpDirectory}/${fileKey}`;
