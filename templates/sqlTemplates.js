b45c66e874bc88c1c3e750f5af525f562d3c3355
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-28 23:11:40 +0200

    Smart object

--- templates/sqlTemplates.js
+++ templates/sqlTemplates.js
@@ -6,22 +6,26 @@
  * @property {string} createTable - SQL template for creating the table
  */
 const sqlTemplates = {
-    createDatabase: `CREATE DATABASE IF NOT EXISTS \`cloudwatch_logs\`;`,
-    useDatabase: `USE \`cloudwatch_logs\`;`,
-    createTable: `${this.useDatabase}; CREATE TABLE IF NOT EXISTS \`logs\` (
-        \`id\` INT AUTO_INCREMENT PRIMARY KEY,
-        \`requestType\` VARCHAR(5) NOT NULL,
-        \`requestUrl\` VARCHAR(5) NOT NULL,
-        \`request_ip\` VARCHAR(50) NOT NULL,
-        \`request_vpc\` VARCHAR(50) NOT NULL,
-        \`request_region\` VARCHAR(50) NOT NULL,
-        \`request_availability_zone\` VARCHAR(50) NOT NULL,
-        \`request_iam_role\` VARCHAR(50) NOT NULL,
-        \`request_api_key\` VARCHAR(50) NOT NULL,
-        \`request_username\` VARCHAR(50) NOT NULL,
-        \`request_authorization_policy\`BOOLEAN NOT NULL DEFAULT FALSE,
-        \`request_scanned\`BOOLEAN NOT NULL DEFAULT FALSE
-    );`
+    createDatabase: '',
+    useDatabase: '',
+    createTable: ''
 };
+
+sqlTemplates.createDatabase = `CREATE DATABASE IF NOT EXISTS \`cloudwatch_logs\`;`;
+sqlTemplates.useDatabase = `USE \`cloudwatch_logs\`;`;
+sqlTemplates.createTable = `${sqlTemplates.useDatabase}; CREATE TABLE IF NOT EXISTS \`logs\` (
+    \`id\` INT AUTO_INCREMENT PRIMARY KEY,
+    \`requestType\` VARCHAR(5) NOT NULL,
+    \`requestUrl\` VARCHAR(5) NOT NULL,
+    \`request_ip\` VARCHAR(50) NOT NULL,
+    \`request_vpc\` VARCHAR(50) NOT NULL,
+    \`request_region\` VARCHAR(50) NOT NULL,
+    \`request_availability_zone\` VARCHAR(50) NOT NULL,
+    \`request_iam_role\` VARCHAR(50) NOT NULL,
+    \`request_api_key\` VARCHAR(50) NOT NULL,
+    \`request_username\` VARCHAR(50) NOT NULL,
+    \`request_authorization_policy\`BOOLEAN NOT NULL DEFAULT FALSE,
+    \`request_scanned\`BOOLEAN NOT NULL DEFAULT FALSE
+);`;
 
 module.exports = { sqlTemplates };
